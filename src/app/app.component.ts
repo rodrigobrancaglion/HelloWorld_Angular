60eff76a6a0a8260d396067b4bd1b3408c09c53a
RODRIGO BRANCAGLION <rodrigo.brancaglion@gmail.com>
2025-02-17 17:04:21 +0100

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'progetto';
+  eta : number = 10;
+  esiste : boolean = false;
+  qualsiasi : any;
+  nomiArray : string[] = [];
+
+  ngOnInit() {
+    this.title = 'progetto';
+    this.eta = 20;
+    this.esiste = false
+    this.qualsiasi = "test";
+    this.nomiArray.push("test");
+
+    this.corsoAngula(222, "test222");
+  }
+
+  corsoAngula(eta: number, nome: string) : boolean {
+    console.log(eta);
+    console.log(nome);
+    return true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+import { Componente1Component } from './components/componente1/componente1.component';
+import { Componente2Component } from './components/componente2/componente2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    NgbModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
