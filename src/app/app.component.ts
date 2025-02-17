57226e417a821b1f75ece9fb8754fc84d8479b9e
RODRIGO BRANCAGLION <rodrigo.brancaglion@gmail.com>
2025-02-17 19:37:47 +0100

    Betalent - Angular - 04 - Data Biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
 
   // TypeScript
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
+    FormsModule,
     MatFormFieldModule,
-    MatInputModule
-  ]
+    MatInputModule,
+  ],
+  exports: [HomeComponent]
 })
-export class MaterialModule { }
+export class HomeModule {}
